9c051c4ca8cc00ba98d714d19dfc49e874042fde
Akshay Machhi <akshay.machhi@investisdigital.com>
2024-01-31 10:15:16 +0000

    push data

---
+++ js/style.js
@@ -0,0 +1,84 @@
+
+/* =============================== Sliding Banner =============================== */
+
+let flag = 0;
+
+function btnclick(x) {
+    flag = flag + x;
+    slideshow(flag);
+    // slider.style.left = "${num+1}%"
+
+}
+slideshow(flag);
+
+function slideshow(num) {
+    let slider = document.getElementsByClassName('banner-sub-block');
+
+    if (num == slider.length) {
+        flag = 0;
+        num = 0;
+    }
+    if (num < 0) {
+        flag = slider.length - 1;
+        num = slider.length - 1;
+    }
+
+    for (let y of slider) {
+        // y.style.display = "none"
+         y.style.opacity = 0;
+
+
+    }
+    // slider[num].style.display = "block";
+    slider[num].style.opacity = 1;
+
+    // slider.style.transform = "translateX(-${flag + 100}%)";
+
+}
+
+/* <!-- --------------------------------------------ACCORDION JS---------------------------------------------- --> */
+
+const accordionItems = document.querySelectorAll('.accordion-box');
+
+accordionItems.forEach(item => {
+    const title = item.querySelector('.accordion-title');
+    const content = item.querySelector('.accordion-description');
+
+    title.addEventListener('click', () => {
+        for (i = 0; i < accordionItems.length; i++) {
+            if (accordionItems[i] == item) {
+                // toggle the accordion item
+                item.classList.toggle('active');
+
+            }
+            else {
+                accordionItems[i].classList.remove('active');
+            }
+        }
+    });
+});
+document.querySelector(".accordion-title").click();
+
+/* <!------------------------------------TABS CONATENT-------------------------------------> */
+
+let tabButtons = document.querySelectorAll(".buttonlinks");
+
+for (let i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener("click", function () {
+        let tabName = this.dataset.tab;
+        let tabContent = document.getElementById(tabName);
+        let allTabContent = document.querySelectorAll(".btncontent");
+        let allTabButtons = document.querySelectorAll(".buttonlinks");
+
+        for (let j = 0; j < allTabContent.length; j++) {
+            allTabContent[j].style.display = "none";
+        }
+
+        for (let k = 0; k < allTabButtons.length; k++) {
+            allTabButtons[k].classList.remove("active");
+        }
+        tabContent.style.display = "block";
+        this.classList.add("active");
+    });
+}
+document.querySelector(".buttonlinks").click();
